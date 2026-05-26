bad450ebaa93fd79d27212b19c9ce6d9477bd0b4
小窗同学 <1614754563@qq.com>
2026-05-27 04:00:46 +0800

    修复：恢复被 XAML 引用的 Converter 和 DataGridWrapPanel

---
+++ src/NarakaBladepoint.StatsAssistant.Framework/Core/Extensions/EnumToLocalizedStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NarakaBladepoint.StatsAssistant.Framework.Core.Extensions
+{
+    /// <summary>
+    /// Converts an enum value to a localized string by looking up
+    /// "{Prefix}{EnumName}" from Application resources.
+    /// ConverterParameter = resource key prefix (e.g. "Mode.")
+    /// </summary>
+    [ValueConversion(typeof(Enum), typeof(string))]
+    public class EnumToLocalizedStringConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return string.Empty;
+
+            var prefix = parameter as string ?? string.Empty;
+            var key = prefix + value.ToString();
+
+            return Application.Current?.TryFindResource(key) as string
+                   ?? value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
