7f5a8822db0e9a84546c8661f228f2d60a4b9e8e
小窗同学 <1614754563@qq.com>
2026-05-28 01:02:43 +0800

    标题栏图标改为 Window.Icon 动态绑定，替换应用图标为刀.ico
    - 标题栏 logo 从硬编码 logo.png 改为 RelativeSource 绑定 Window.Icon
    - 新增 WindowIconConverter 转换器
    - 替换 app.ico 为刀.ico（E:\下载\刀.ico）

---
+++ src/BlackGoldAncientSword.Framework/Core/Extensions/WindowIconConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BlackGoldAncientSword.Framework.Core.Extensions
+{
+    /// <summary>
+    /// 将 Window.Icon 绑定到 Image.Source 的转换器。
+    /// 直接返回 Window 图标，无需额外转换；
+    /// 若图标为 null 则返回 null。
+    /// </summary>
+    [ValueConversion(typeof(ImageSource), typeof(ImageSource))]
+    public class WindowIconConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value as ImageSource;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
